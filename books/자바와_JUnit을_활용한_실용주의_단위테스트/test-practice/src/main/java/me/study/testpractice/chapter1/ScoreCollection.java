da7ad6127dfee27c71054aabb00cfbe3e5c9bf1f
Cho-D-YoungRae <yrc9229@gmail.com>
2022-04-03 14:07:40 +0900

    ONGOING: 자바와 JUnit을 활용한 실용주의 단위테스트 chapter 1

---
+++ books/자바와_JUnit을_활용한_실용주의_단위테스트/test-practice/src/main/java/me/study/testpractice/chapter1/ScoreCollection.java
@@ -0,0 +1,16 @@
+package me.study.testpractice.chapter1;
+
+import java.util.*;
+
+public class ScoreCollection {
+    private final List<Scoreable> scores = new ArrayList<>();
+
+    public void add(Scoreable scoreable) {
+        scores.add(scoreable);
+    }
+
+    public int arithmeticMean() {
+        int total = scores.stream().mapToInt(Scoreable::getScore).sum();
+        return total / scores.size();
+    }
+}
---
+++ books/자바와_JUnit을_활용한_실용주의_단위테스트/test-practice/src/main/java/me/study/testpractice/chapter1/Scoreable.java
@@ -0,0 +1,6 @@
+package me.study.testpractice.chapter1;
+
+@FunctionalInterface
+public interface Scoreable {
+    int getScore();
+}
