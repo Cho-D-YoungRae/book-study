cbd9b5786274b296a6bb61ff3daafbf72af613ca
Cho-D-YoungRae <yrc9229@gmail.com>
2022-09-22 21:10:48 +0900

    Vue.js 프로젝트 투입 일주일 전: chapter5

---
+++ books/Vue_js_프로젝트_투입_일주일_전/vue-project-practice/src/views/DataBindingStyle2.vue
@@ -0,0 +1,19 @@
+<template>
+<div :style="[baseStyle, addStyle]">인라인 스타일 바인딩</div>
+</template>
+
+<script>
+export default {
+  name: "DataBindingStyle",
+  data() {
+    return {
+      baseStyle: 'background-color:yellow;width:100%;height:200px;',
+      addStyle: 'color:red;font-weight:bold;'
+    };
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
