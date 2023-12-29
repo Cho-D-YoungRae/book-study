5b76c950b7b9b36d8d5a0852d47d3d40cfe8c1c7
Cho Youngrae <yrc9229@gmail.com>
2023-12-30 04:51:58 +0900

    스프링 마이크로서비스 코딩 공작소: 5

--- books/스프링 마이크로서비스 코딩 공작소/spring-msa-coding/license-service/src/main/java/com/example/licenseservice/model/License.java
+++ books/스프링 마이크로서비스 코딩 공작소/spring-msa-coding/license-service/src/main/java/com/example/licenseservice/model/License.java
@@ -1,15 +1,37 @@
 package com.example.licenseservice.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import org.springframework.hateoas.RepresentationModel;
 
+@Entity
+@Table(name = "licenses")
 @Data
 public class License extends RepresentationModel<License> {
 
-    private int id;
+    @Id
+    @Column(name = "license_id", nullable = false)
     private String licenseId;
+
     private String description;
+
+    @Column(name = "organization_id", nullable = false)
     private String organizationId;
+
+    @Column(name = "product_name", nullable = false)
     private String productName;
+
+    @Column(name = "license_type", nullable = false)
     private String licenseType;
+
+    @Column(name="comment")
+    private String comment;
+
+    public License withComment(String comment){
+        this.setComment(comment);
+        return this;
+    }
 }
