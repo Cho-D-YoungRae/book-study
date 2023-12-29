6e2de080cf0b6018c2326710f99f97e6d7c2a4a2
Cho Youngrae <yrc9229@gmail.com>
2023-12-29 14:17:52 +0900

    스프링 마이크로서비스 코딩 공작소: 2

---
+++ books/스프링 마이크로서비스 코딩 공작소/spring-msa-coding/src/main/java/com/example/springmsacoding/SpringMsaCodingApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmsacoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMsaCodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMsaCodingApplication.class, args);
+    }
+
+}
---
+++ books/스프링 마이크로서비스 코딩 공작소/spring-msa-coding/src/test/java/com/example/springmsacoding/SpringMsaCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmsacoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMsaCodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
