064ff2f4f188768fe35753cdc105bb2984619373
Youngrae Cho <yrc9229@gmail.com>
2025-04-20 21:04:27 +0900

    아파치 카프카 애플리케이션 프로그래밍: 3-1~3-4

---
+++ books/아파치 카프카 애플리케이션 프로그래밍/kafka-study-2/src/main/java/com/example/kafkastudy/KafkaStudy2Application.java
@@ -0,0 +1,13 @@
+package com.example.kafkastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStudy2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaStudy2Application.class, args);
+    }
+
+}
---
+++ books/아파치 카프카 애플리케이션 프로그래밍/kafka-study-2/src/main/java/com/example/kafkastudy/chapter03/SimpleConsumer_4.java
@@ -0,0 +1,43 @@
+package com.example.kafkastudy.chapter03;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+public class SimpleConsumer_4 {
+
+    private static final Logger log = LoggerFactory.getLogger(SimpleConsumer_4.class);
+    private static final String TOPIC_NAME = "test";
+    private static final String BOOTSTRAP_SERVERS = "localhost:29092,localhost:39092,localhost:49092";
+    private static final String GROUP_ID = "test-group-2";
+
+    public static void main(String[] args) {
+        Properties configs = new Properties();
+        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        configs.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        configs.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+
+        // 기본 설정으로 생성할 수 있는 오토 커밋 컨슈머
+        try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(configs)) {
+            consumer.subscribe(List.of(TOPIC_NAME));
+            while (true) {
+                // duration 값으로 브로커로부터 데이터를 가져올 때 컨슈머 버퍼에 데이터를 기다리기 윟나 타임아웃 간격 설정
+                ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofSeconds(1));
+                for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
+                    log.info("record: {}", consumerRecord);
+                }
+                consumer.commitSync();
+            }
+        }
+    }
+}
